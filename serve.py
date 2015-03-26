642fb6a23427752d0d23fb94842d36d24a289795
Phil Howard <phil@gadgetoid.com>
2015-03-26 14:10:59 +0000

    Added local test server

---
+++ serve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from flask import Flask, send_from_directory
+import sys
+app = Flask(__name__)
+
+lang = 'en-GB'
+
+@app.route("/resources/<path:filename>")
+def custom_static(filename):
+	return send_from_directory(basedir + 'resources/', filename)
+
+@app.route("/<path:page>")
+def show_page(page):
+	return send_from_directory(basedir,'{}.html'.format(page))
+
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1:
+		lang = sys.argv[1]
+
+	basedir = 'output/{lang}/'.format(lang=lang)
+
+	app.run(host='0.0.0.0', debug=True)

3933d59867e9a5ae3347882b2f37195655b92e65
RogueM <chaosgen6@googlemail.com>
2016-06-27 00:22:51 +0100

    digiamp for review + append script

---
+++ draft/append.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+draftmd="../draft/overlay"
+draftpng="../draft/boards"
+dirmd="../src/en/overlay"
+dirpng="../resources/boards"
+tmpmd="$dirmd/template.md"
+tmppng="$dirpng/template.png"
+yamlfile="../src/en/settings.yaml"
+mdlist=$(ls "$draftmd")
+
+cp -R $draftmd/* $dirmd && rm $tmpmd
+cp -R $draftpng/* $dirpng && rm $tmppng
+
+for overlay in $mdlist; do
+    if [ $overlay != "template.md" ]; then
+        board=$(echo "$overlay" | rev | cut -c 4- | rev)
+        if ! grep -e $board $yamlfile &> /dev/null; then
+            echo "- $board" | tee -a $yamlfile &> /dev/null
+        fi
+    fi
+done
+
+exit 0

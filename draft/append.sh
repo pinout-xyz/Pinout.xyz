674cfe155bef4164dd281399bebbb4f4e3a8edf1
RogueM <chaosgen6@googlemail.com>
2016-07-09 12:58:55 +0100

    full merge from draft

--- draft/append.sh
+++ draft/append.sh
@@ -9,15 +9,14 @@ tmppng="$dirpng/template.png"
 yamlfile="../src/en/settings.yaml"
 mdlist=$(ls "$draftmd")
 
-mv -R $draftmd/* $dirmd && rm $tmpmd
-mv -R $draftpng/* $dirpng && rm $tmppng
-
 for overlay in $mdlist; do
     if [ $overlay != "template.md" ]; then
         board=$(echo "$overlay" | rev | cut -c 4- | rev)
         if ! grep -e $board $yamlfile &> /dev/null; then
             echo "- $board" | tee -a $yamlfile &> /dev/null
         fi
+        mv $draftmd/$overlay $dirmd
+        mv $draftpng/$board.png $dirpng
     fi
 done
 

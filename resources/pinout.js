922318fc32e1784efc987e3cd693ef08e9641c66
Gadgetoid <Gadgetoid@users.noreply.github.com>
2025-12-07 12:47:07 +0000

    deploy: a6f1bda700d2d4b56fb5b9438feb1f30ecece603

--- resources/pinout.js
+++ resources/pinout.js
@@ -1,70 +1,8 @@
 jQuery(document).ready(function(){
-	var overlay_slideUp;
-	var dropdowns = $('#sections ul li .dropdown');
 
 	$('pre').addClass('prettyprint').addClass('linenums');
 
 	window.prettyPrint&&prettyPrint();
-
-	var groups = $('#sections .boards .group');
-	var group_nav = $('#sections .boards .group-nav li');
-	
-	groups.hide().filter(':eq(0)').show();
-	group_nav.removeClass('active').filter(':eq(0)').addClass('active');
-
-	group_nav.on('click',function(e){
-		group_nav.removeClass('active');
-		$(this).addClass('active');
-		groups.hide();
-
-		var group = $(this).data('group');
-
-		groups.filter('.group_' + group).show();
-	})
-
-	$('#container').on('click',function(){
-		dropdowns.slideUp(100);
-	});
-
-	$('#sections > ul > li > a').on('click',function(e){
-		e.preventDefault();
-		e.stopPropagation();
-		var dropdown = $(this).parent().find('.dropdown');
-		dropdowns.hide();
-		dropdown.show();
-	});
-
-	$('#sections > ul > li > a').hover(function(e){
-		e.preventDefault();
-		var dropdown = $(this).parent().find('.dropdown');
-		clearTimeout(overlay_slideUp);
-		if(dropdowns.filter(':visible').length){
-			dropdowns.hide();
-			dropdown.show();
-		}
-		else
-		{
-			overlay_slideUp = setTimeout(function(){dropdowns.slideUp(100);dropdown.slideDown(100);}, 300);
-		}
-	},function(e){
-		e.preventDefault();
-		var dropdown = $(this).parent().find('.dropdown');
-		clearTimeout(overlay_slideUp);
-		overlay_slideUp = setTimeout(function(){dropdown.slideUp(100);}, 100);
-	});
-
-	dropdowns.on('click',function(e){
-		e.stopPropagation();
-	})
-
-	dropdowns.hover(function(e){
-		clearTimeout(overlay_slideUp);
-	},function(e){
-		e.preventDefault();
-		var dropdown = $(this);
-		clearTimeout(overlay_slideUp);
-		overlay_slideUp = setTimeout(function(){dropdown.slideUp(100);}, 100);
-	})
 
 	$('article p,article li').each(function(){
 		html = $(this).html();
@@ -79,7 +17,7 @@ jQuery(document).ready(function(){
 		html = html.replace(
 			/GPIO\ ([0-9]{1,2})/gi,
 			function(str, c1){
-				var pin = $("#gpio li").filter(function(){return $(this).find("span.name").text() == "GPIO " + c1}).find('.phys').text();
+				var pin = $("#gpio li").filter(function(){return $(this).find("span.name").text() == "GPIO " + c1}).find('.pin').text();
 				return '<span title="Click for details about pin ' + pin + '" class="pin-hover" data-pin="' + pin + '">' + str + '</span>';
 			}
 		)

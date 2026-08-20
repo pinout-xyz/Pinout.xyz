a381cea5838ecb0ebc0cd7cb8129946b75c00ad3
Phil Howard <github@gadgetoid.com>
2026-08-20 18:49:32 +0100

    pinrefs: Match the GPIOn and GPIOn/n forms the old regex missed

--- pinoutxyz/pinrefs.py
+++ pinoutxyz/pinrefs.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ElementTree
 from markdown.extensions import Extension
 from markdown.treeprocessors import Treeprocessor
 
-REFERENCE = re.compile(r'Physical Pin (\d{1,2})|GPIO (\d{1,2})', re.IGNORECASE)
+REFERENCE = re.compile(r'Physical Pin (\d{1,2})(?!\d)|GPIO ?(\d{1,2})(?:/(\d{1,2}))?(?!\d)',
+                       re.IGNORECASE)
 OPAQUE = ('code', 'pre')
 TITLE = 'Click for details about pin {}'
 
@@ -14,11 +15,37 @@ class PinReferences(Treeprocessor):
         super().__init__(md)
         self.pins = pins
 
-    def physical(self, match):
-        physical, bcm = match.group(1), match.group(2)
+    def span(self, text, pin, titled):
+        span = ElementTree.Element('span')
+        if titled:
+            span.set('title', TITLE.format(pin))
+        span.set('class', 'pin-hover')
+        span.set('data-pin', pin)
+        span.text = text
+        return span
+
+    def nodes(self, match):
+        physical, bcm, paired = match.group(1), match.group(2), match.group(3)
+
         if physical is not None:
-            return physical if physical in self.pins else None
-        return self.pins.bcm_to_physical(bcm)
+            if physical not in self.pins:
+                return [], match.start()
+            return [self.span(match.group(0), physical, False)], match.end()
+
+        pin = self.pins.bcm_to_physical(bcm)
+        if pin is None:
+            return [], match.start()
+
+        text = match.group(0)
+        partner = None if paired is None else self.pins.bcm_to_physical(paired)
+
+        if partner is None:
+            head = text if paired is None else text[:text.rindex('/' + paired)]
+            return [self.span(head, pin, True)], match.start() + len(head)
+
+        head = self.span(text[:text.rindex('/' + paired)], pin, True)
+        head.tail = '/'
+        return [head, self.span(paired, partner, True)], match.end()
 
     def spans(self, text):
         found = []
@@ -26,8 +53,8 @@ class PinReferences(Treeprocessor):
         position = 0
 
         for match in REFERENCE.finditer(text):
-            pin = self.physical(match)
-            if pin is None:
+            nodes, consumed = self.nodes(match)
+            if not nodes:
                 continue
 
             chunk = text[position:match.start()]
@@ -36,14 +63,8 @@ class PinReferences(Treeprocessor):
             else:
                 leading = chunk
 
-            span = ElementTree.Element('span')
-            if match.group(2) is not None:
-                span.set('title', TITLE.format(pin))
-            span.set('class', 'pin-hover')
-            span.set('data-pin', pin)
-            span.text = match.group(0)
-            found.append(span)
-            position = match.end()
+            found += nodes
+            position = consumed
 
         if not found:
             return None, []
